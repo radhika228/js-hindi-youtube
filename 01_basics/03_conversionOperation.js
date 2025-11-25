695d4af8d421f78205f99673addf162fd3eb6e6f
radhika228 <radhikagupta0891@gmail.com>
2025-11-25 19:42:10 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,10 +13,36 @@ let score="33abc"
 
  let isLoggedIn= 1 
  let booleanisLoggedIn= Boolean(isLoggedIn)
- console.log(booleanisLoggedIn)
+ //console.log(booleanisLoggedIn)
 
  //1 => true , 0=> false
  //"" => false
  //"radhika" => true
 
  //Just like we are converting into Number and Boolean , we can also convert into String 
+
+ //*********************************Operations******************************************
+
+ let value =3
+ let negValue=-value
+ console.log(negValue) //-3
+
+ //console.log(2+2)
+ //console.log(2-2)
+ //console.log(2*2)
+//console.log(2**2) //power
+//console.log(2/2)
+//console.log(2%2)
+
+//let str1="hello"
+//let str2="world"
+//let str3=str1+str2
+//console.log(str3)
+
+//console.log("1" + 2)       //12
+//console.log(1 + "2")       //12
+//console.log("1" + 2 + 2)   //122
+//console.log(1 + 2 + "2")   //32 //depends on which value comes first
+
+
+console.log(+true) //1
