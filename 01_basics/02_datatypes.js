f4bfd10e82eb8df4766e5af72f397152f9ccad13
radhika228 <radhikagupta0891@gmail.com>
2025-11-25 18:35:39 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) error came because we are using node js not browser
+console.log(3 
+    +
+     3)// it will run but code readability should be high so we should write properly
+
+ console.log("Radhika")
+
+
+ let name = "radhika"
+ let age=20
+ let isLoggedIn = false 
+ let state;
+
+ //number => 2 to power 53
+ //bigint
+ //string=>" "
+ //boolean =>true/false
+ //null =>standalone value
+ //undefined=>
+ //symbol =>unique
+
+
+ //object
+
+console.log(typeof null)        //object
+console.log(typeof undefined)  //undefined
