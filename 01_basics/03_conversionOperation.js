6ba561b49a1b75455de78801f7333d7cccf13cce
radhika228 <radhikagupta0891@gmail.com>
2025-11-25 19:09:49 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33abc"
+
+// console.log(typeof score); 
+// console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber); //number
+ //console.log( valueInNumber); //NaN
+
+ //"33" => 33
+ //"33abc" => NaN
+ //true=> 1; false=> 0
+
+ let isLoggedIn= 1 
+ let booleanisLoggedIn= Boolean(isLoggedIn)
+ console.log(booleanisLoggedIn)
+
+ //1 => true , 0=> false
+ //"" => false
+ //"radhika" => true
+
+ //Just like we are converting into Number and Boolean , we can also convert into String 
