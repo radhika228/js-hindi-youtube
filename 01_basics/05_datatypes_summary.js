2eedd7cb0d26f2dde40a03fa0cdf6b3e5652b02d
radhika228 <radhikagupta0891@gmail.com>
2025-11-25 20:01:13 +0000

    datatype summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,12 @@
+//data types
+//1 primitive : 7 types => String ,Number ,Boolean, null, undefined ,Symbol, BigInt
+//2 reference (non primitive) : Arrays, Objects, Functions
+
+const id = Symbol('123')  //example of symbol primitive datatype
+const bignumber = 4677878778899009n //example of bigint primitive datatype
+
+
+//function below
+const myFunction = function(){
+    console.log("hello world")
+}
