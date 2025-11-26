6e33b096ec704687c1111f9c836dbbd2a9021685
radhika228 <radhikagupta0891@gmail.com>
2025-11-26 19:08:48 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,25 @@
+const name = "abc"
+const repo = 40
+
+//console.log(name + repo + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+const gameName = new String ('radhikaaa')
+console.log(gameName[0]); //r
+console.log(gameName.__proto__); //{}
+  //.length
+  //.toUpperCase()
+  //.toLowerCase()
+// charAt(0) 
+// indexOf('r')  
+//.substring(0,4) // 0 to 3
+//.slice //also take negative
+//.trim
+//trim javascript mdn visit
+
+//.replace
+//.includes //true or false
+//.split()
+
+//read them all
