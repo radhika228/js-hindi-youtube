e917baf827a03886a040bd9ce5d1ad3351d6ede6
radhika228 <radhikagupta0891@gmail.com>
2025-12-06 14:09:51 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -11,9 +11,14 @@ const myArr = [0, 1, 2, 3, 4, 5]
 //myArr.includes(9)
 //myArr.indexOf(3)
 
-const newArr = myArr.join()
-console.log(myArr);  // [0, 1, 2, 3, 4, 5]
-console.log(newArr); //0,1,2,3,4,5
+//const newArr = myArr.join()
+//console.log(myArr);  // [0, 1, 2, 3, 4, 5]
+//console.log(newArr); //0,1,2,3,4,5
 
 //+++++++++++++++++++  slice and splice ++++++++++++++++++++
 
+console.log(myArr.slice(1,3));   // [ 1, 2 ]
+
+let arr = [1, 2, 3, 4];
+arr.splice(1, 2, 99, 100);
+console.log(arr); // [1, 99, 100, 4]
