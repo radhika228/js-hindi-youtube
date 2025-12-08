0f2be747bf22a4438c4f6baf34520fb87175bd31
radhika228 <radhikagupta0891@gmail.com>
2025-12-08 15:45:14 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,8 +1,21 @@
+
 // singleton -> constructor se banega ,literal se nhi
 //object.create (constructor)
 
 
 //object literals
-const JsUser = {
-    name : "Sam"
+const JsUser = {        //object declaration
+    name : "Sam",
+    "fullname":"Sam Tiwari",
+    age : 19,
+    location : "Bhopal",
+    mail : "sam@google.com",
+    isLoggedin : false,
+    lastlogin : ["Monday","Saturday"]
 }
+
+
+//object access
+console.log(JsUser.mail);
+console.log(JsUser["mail"]); //use this always
+console.log(JsUser["fullname"])
