ce8df6a70a5f32cb5b299543824209fe8056d493
radhika228 <radhikagupta0891@gmail.com>
2025-12-09 18:44:06 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,9 +4,13 @@
 
 
 //object literals
+
+const mySym = Symbol("key1")
+
 const JsUser = {        //object declaration
     name : "Sam",
     "fullname":"Sam Tiwari",
+    [mySym] : "myKey1",
     age : 19,
     location : "Bhopal",
     mail : "sam@google.com",
@@ -19,3 +23,6 @@ const JsUser = {        //object declaration
 console.log(JsUser.mail);
 console.log(JsUser["mail"]); //use this always
 console.log(JsUser["fullname"])
+console.log(JsUser[mySym])
+
+Object.freeze(JsUser)  // after this no changes will be applied
