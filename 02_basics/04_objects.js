63f6c56956817cca7586f1713576848646eeca4b
radhika228 <radhikagupta0891@gmail.com>
2025-12-11 17:45:40 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,3 +29,14 @@ const regularUser = {
 console.log(regularUser.fullName);  // { userfullname: { firstName: 'Ram', lastName: 'Gupta' } } 
 
 console.log(regularUser.fullName.userfullname.firstName); // Ram
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const obj1 = {1:"a" , 2: "b"}
+const obj2 = {3: "a" , 4: "b"}
+
+//const obj3 = {obj1 , obj2}
+//console.log(obj3); // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } 
+
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
