4abdf80c380effb461f14f10916a6cc52fcce161
radhika228 <radhikagupta0891@gmail.com>
2025-12-18 07:03:03 +0000

    settimeout

---
+++ 03_/callBack&setTimeout.js
@@ -0,0 +1,9 @@
+//callBack function-> a function which passes in other function
+
+//setTimeout function
+//print 1 to 10 but with delay of 1 sec after each number gets printed
+/*for(int i =1 ;i<=10;i++){
+    setTimeout(function()){
+        console.log(i);
+    },i*1000)
+}*/
