12a9627146e2d987f0654d16eb76af79e2062ed0
radhika228 <radhikagupta0891@gmail.com>
2025-12-18 07:37:10 +0000

    sort

--- 03_movingForward/sort.js
+++ 03_movingForward/sort.js
@@ -3,5 +3,13 @@ let arr=[5,7,5,34,2,7,4235,9];
 console.log(arr);
 arr= arr.sort();
 console.log(arr);
+//the array  will get sorted in output in inc order
 
-//the array  will get sorted in output
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let arr=[5,7,5,34,2,7,4235,9];
+console.log(arr);
+arr= arr.sort((a,b)=>b-a);
+console.log(arr);
+// array will get sorted in dec order
+
